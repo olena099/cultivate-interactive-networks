7f68e00fd25e68127546238d8e16838d045fbd78
olena099 <142909583+olena099@users.noreply.github.com>
2023-09-27 09:34:16 -0700

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,51 @@
+if __name__ == "__main__":
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    main()
+def generate_random_data():
+
+
+import random
+
+    return data
+def main():
+    data = generate_random_data()
+        print(f"Random Number: {item}")        for (int item : data) {
+        int[] data = new int[10];
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+
+public class RandomDataGenerator {
+}
+            System.out.println("Random Number: " + item);
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        }    return data
+
+import random
+def generate_random_data():
+
+
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    main()
+
+def main():
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()    main()
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+
+import random
+
+def main():
+
+    return data
+if __name__ == "__main__":
+def generate_random_data():
+    data = generate_random_data()
