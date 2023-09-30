a15d8947946eab7923f3f568bbb1afe9f18ea700
olena099 <142909583+olena099@users.noreply.github.com>
2023-09-29 22:02:05 -0700

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,72 @@
+    for item in data:
+    data = generate_random_data()
+    main()
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+def main():
+if __name__ == "__main__":
+import random
+def generate_random_data():
+
+    return data
+
+    return data
+        print(f"Random Number: {item}")
+def generate_random_data():
+    for item in data:
+
+import random
+if __name__ == "__main__":
+    main()
+
+def main():
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]    });
+    const data = [];
+
+    data.forEach(item => {
+}
+
+    for (let i = 0; i < 10; i++) {
+    return data;
+
+function main() {
+}
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+function generateRandomData() {    return data
+
+    data = generate_random_data()
+
+    main()
+
+def generate_random_data():
+def main():
+    for item in data:
+import random
+if __name__ == "__main__":
+
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")    return data;
+
+}
+
+    data.forEach(item => {
+main();
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+
+    const data = [];
+    });
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    }
+function generateRandomData() {
