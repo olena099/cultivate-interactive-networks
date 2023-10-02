e9fed2231a70096973e0f6cc2960bf2ca46eb50d
olena099 <142909583+olena099@users.noreply.github.com>
2023-10-02 10:26:08 -0700

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,26 +1,14 @@
-        print(f"Random Number: {item}")
-import random
-    data = generate_random_data()
-def generate_random_data():
 
-
-    data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":
-
-    main()
-    for item in data:
-def main():
-    return data
-import random
-    for item in data:
         print(f"Random Number: {item}")
 
-    main()
-    data = generate_random_data()
-def generate_random_data():
+
+
+    for item in data:
     data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":
-
 def main():
+    data = generate_random_data()
     return data
-
+if __name__ == "__main__":
+import random
+    main()
+def generate_random_data():
